bccbda22f40fd0a9220f12d1d06d3283fecf7c6b
Shiro Vulpes <Vulpes.Shiro@gmail.com>
2023-09-09 20:04:06 +0200

    ok

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: dlacuey <dlacuey@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/07/10 21:05:04 by dlacuey           #+#    #+#              #
-#    Updated: 2023/08/30 01:00:01 by dlacuey          ###   ########.fr        #
+#    Updated: 2023/09/09 18:51:13 by dlacuey          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,17 +14,26 @@ CC = gcc
 
 CFLAGS = -Wall -Wextra -Werror
 
+CPPFLAGS = -Ilibft/ -Iget_next_line/
+
 OBJS =												\
 													\
 				main.o								\
+				$(addprefix get_next_line/,			\
+				get_next_line.o						\
+				get_next_line_utils.o)				\
 													\
 
 NAME = minishell
+LIBFT = libft/libft.a
 
 all: $(NAME)
 
-$(NAME): $(OBJS)
-	$(CC) -o $(NAME) $(OBJS) -lreadline
+$(LIBFT):
+	make -C libft/
+
+$(NAME): $(OBJS) $(LIBFT)
+	$(CC) -o $(NAME) $^ -lreadline
 
 clean:
 	$(RM) $(OBJS) $(OBJS_BONUS)
@@ -33,5 +42,8 @@ fclean: clean
 	$(RM) $(NAME)
 
 re: fclean all
+
+check: all
+	pharaoh tests/tests_e2e/
 
 .PHONY: all clean fclean re bonus
--- main.c
+++ main.c
@@ -2,18 +2,72 @@
 #include <stdio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include "libft/libft.h"
+#include <sys/wait.h>
+#include <fcntl.h>
+#include "get_next_line/get_next_line.h"
 
-int main(/* int argc, char **argv, char **envp */)
+extern char **environ;
+
+static bool is_interactive_mode(void)
 {
-	char	*input;
+	if (isatty(STDIN_FILENO))
+		return true;
+	return false;
+}
 
-	while(1)
+static void exec_command(char *input)
+{
+	char	**argv;
+	pid_t	pid1;
+
+	argv = ft_split(input, ' ');
+	pid1 = fork();
+	if (pid1 == 0)
+		execve(argv[0], argv, environ);
+	waitpid(pid1, NULL, 0);
+}
+
+void	interactive_mode(void)
+{
+	char *input;
+
+	while(true)
 	{
 		input = readline("Wesh: ");
 		if (!input)
 			break;
 		add_history(input);
+		exec_command(input);
+		free(input);
 	}
-	free(input);
+}
+
+void	non_interactive_mode(void)
+{
+	char *input = NULL;
+
+	while(true)
+	{
+		printf("%p\n", input);
+		input = get_next_line(STDIN_FILENO);
+		printf("%p\n", input);
+		if (!input)
+			break;
+		printf("%p\n", input);
+		add_history(input);
+		exec_command(input);
+		free(input);
+	}
+}
+
+int main(void)
+{
+	if (is_interactive_mode())
+		interactive_mode();
+	else
+		non_interactive_mode();
 	return (0);
 }
