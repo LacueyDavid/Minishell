6b9d1cc296a22aa22c92563d8de5b12dcde8a9d0
Shiro Vulpes <Vulpes.Shiro@gmail.com>
2023-09-09 23:30:34 +0200

    ok

---
+++ sources/minishell/execution/execution.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execution.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlacuey <dlacuey@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/09 21:53:01 by dlacuey           #+#    #+#             */
+/*   Updated: 2023/09/09 23:21:32 by dlacuey          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <sys/wait.h>
+#include "libft.h"
+
+extern char **environ;
+
+void exec_command(char *input)
+{
+	char	**argv;
+	pid_t	pid1;
+	char	**paths;
+	char	*command;
+
+	argv = ft_split(input, ' ');
+	paths = find_paths(environ);
+	command = get_command(argv[0], paths);
+	pid1 = fork();
+	if (pid1 == 0)
+		execve(command, argv, environ);
+	waitpid(pid1, NULL, 0);
+}
---
+++ sources/minishell/minishell.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlacuey <dlacuey@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/09 21:58:22 by dlacuey           #+#    #+#             */
+/*   Updated: 2023/09/09 23:23:49 by dlacuey          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include "libft.h"
+#include "execution.h"
+#include "get_next_line.h"
+
+static void	interactive_mode(void)
+{
+	char *input;
+
+	while(true)
+	{
+		input = readline("Wesh: ");
+		if (!input)
+			break;
+		add_history(input);
+		exec_command(input);
+		free(input);
+	}
+}
+
+static void	non_interactive_mode(void)
+{
+	char *input;
+
+	while(true)
+	{
+		input = get_next_line(STDIN_FILENO);
+		if (!input)
+			break;
+		delete_newline(&input);
+		exec_command(input);
+		free(input);
+	}
+}
+
+void	minishell(void)
+{
+	if (is_interactive_mode())
+		interactive_mode();
+	else
+		non_interactive_mode();
+}
