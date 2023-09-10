f7031d265f065bec6bd577a4b4472f1a8230a620
Shiro Vulpes <Vulpes.Shiro@gmail.com>
2023-09-10 22:26:32 +0200

    ok

--- sources/minishell/lexing/test_lexer.c
+++ sources/minishell/lexing/test_lexer.c
@@ -18,3 +18,19 @@ Ensure(lexer, recognizes_a_word)
 	assert_that(token_list->tokens[0].value, is_equal_to_string("hello"));
 	destroy_token_list(token_list);
 }
+
+Ensure(lexer, recognizes_multiple_tokens)
+{
+	//GIVEN
+	char *input = "hello world!";
+	t_token_list *token_list = NULL;
+	//WHEN
+	token_list = lexer(input);
+	//THEN
+	assert_that(token_list->size, is_equal_to(2));
+	assert_that(token_list->tokens[0].type, is_equal_to(WORD));
+	assert_that(token_list->tokens[0].value, is_equal_to_string("hello"));
+	assert_that(token_list->tokens[1].type, is_equal_to(WORD));
+	assert_that(token_list->tokens[1].value, is_equal_to_string("world!"));
+	destroy_token_list(token_list);
+}
