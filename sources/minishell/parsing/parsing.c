db2512dd2db5de63e83c1eeda8180ea2b9e953b7
David LACUEY <dlacuey@bess-f2r4s4.clusters.42paris.fr>
2023-10-18 02:03:32 +0200

    ok

---
+++ sources/minishell/parsing/parsing.c
@@ -0,0 +1,5 @@
+#include "parsing.h"
+#include "lexer.h"
+
+
+
--- sources/minishell/parsing/parsing.h
+++ sources/minishell/parsing/parsing.h
@@ -1,19 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   execution.h                                        :+:      :+:    :+:   */
+/*   parsing.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dlacuey <dlacuey@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/09 22:03:39 by dlacuey           #+#    #+#             */
+/*   Created: 2023/10/18 02:00:47 by dlacuey           #+#    #+#             */
 /*   Updated: 2023/10/18 02:00:56 by dlacuey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef EXECUTION_H
-# define EXECUTION_H
+#ifndef PARSING_H
+# define PARSING_H
 
-void	exec_full_command(t_node *node);
-void	exec_simple_command(char *input);
+#include "lexer.h"
+
+typedef struct s_node
+{
+	e_token_type	type;
+	char			*value;
+	struct s_node	*left;
+	struct s_node	*right;
+}	t_node;
 
 #endif
