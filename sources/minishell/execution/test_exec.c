051256e91a5a00fb2962ac02b60707f39862d458
David LACUEY <dlacuey@bess-f2r1s19.clusters.42paris.fr>
2023-10-21 03:17:29 +0200

    fin des tests echo toto > outfile

--- sources/minishell/execution/test_exec.c
+++ sources/minishell/execution/test_exec.c
@@ -46,3 +46,81 @@ Ensure(exec_full_command, can_exec_echo_toto)
 	free(line);
 	fclose(file);
 }
+
+Ensure(exec_full_command, can_exec_echo_toto_with_option)
+{
+	const char *output_file = "test.txt";
+	char *line;
+
+	//GIVEN (etant donné etat initial)
+	t_node *node = malloc(sizeof(t_node));
+	node->type = SIMPLE_COMMAND;
+	node->value = malloc(sizeof(char *) * 4);
+	node->value[0] = ft_strdup("echo");
+	node->value[1] = ft_strdup("-n");
+	node->value[2] = ft_strdup("toto");
+	node->value[3] = NULL;
+	node->left = NULL;
+	node->right = NULL;
+	//WHEN (quand)
+	// int fd = open(output_file, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	// dup2(fd, STDOUT_FILENO);
+	FILE *file = fopen(output_file, "w");
+	freopen(output_file, "w", stdout);
+	exec_full_command(node);
+
+	//THEN (alors)
+	int fd = open(output_file, O_RDONLY);
+	line = get_next_line(fd);
+	assert_that(line, is_equal_to_string("toto"));
+	close(fd);
+	unlink(output_file);
+
+	free_strs(node->value);
+	free(node);
+	free(line);
+	fclose(file);
+}
+
+Ensure(exec_full_command, can_exec_echo_toto_with_redirection)
+{
+	const char *output_file = "outfile.txt";
+	char *line;
+
+	//GIVEN (etant donné etat initial)
+	t_node *left = malloc(sizeof(t_node));
+	left->type = SIMPLE_COMMAND;
+	left->value = malloc(sizeof(char *) * 4);
+	left->value[0] = ft_strdup("echo");
+	left->value[1] = ft_strdup("-n");
+	left->value[2] = ft_strdup("toto");
+	left->value[3] = NULL;
+
+	t_node *right = malloc(sizeof(t_node));
+	right->type = SIMPLE_COMMAND;
+	right->value = malloc(sizeof(char *) * 2);
+	right->value[0] = ft_strdup(output_file);
+	right->value[1] = NULL;
+
+	t_node *head = malloc(sizeof(t_node));
+	head->type = O_REDIRECTION;
+	head->left = left;
+	head->right = right;
+
+	//WHEN (quand)
+	exec_full_command(head);
+
+	//THEN (alors)
+	int fd = open(output_file, O_RDONLY);
+	line = get_next_line(fd);
+	assert_that(line, is_equal_to_string("toto"));
+	close(fd);
+	unlink(output_file);
+
+	free_strs(left->value);
+	free(left);
+	free_strs(right->value);
+	free(right);
+	free(head);
+	free(line);
+}
