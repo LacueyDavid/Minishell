0d330eb35bf2d12374ccdf871e5dda12288502b1
David LACUEY <dlacuey@bess-f2r1s19.clusters.42paris.fr>
2023-10-24 03:20:07 +0200

    finalisation de la protection des syscall

--- sources/minishell/execution/execution.c
+++ sources/minishell/execution/execution.c
@@ -6,7 +6,7 @@
 /*   By: jdenis <jdenis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 21:53:01 by dlacuey           #+#    #+#             */
-/*   Updated: 2023/10/23 09:49:02 by dlacuey          ###   ########.fr       */
+/*   Updated: 2023/10/24 03:19:14 by dlacuey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include "execution.h"
 #include "fcntl.h"
 #include <sys/wait.h>
+#include <stdio.h>
 
 extern char	**environ;
 
@@ -26,6 +27,7 @@ void	exec_simple_command(char **values)
 	paths = find_paths(environ);
 	command = get_command(values[0], paths);
 	execve(command, values, environ);
+	(perror("Command not found"), exit(1));
 }
 
 void	redirection_output(t_node *node)
@@ -33,8 +35,12 @@ void	redirection_output(t_node *node)
 	int	fd;
 
 	fd = open(node->right->values[0], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	dup2(fd, 1);
-	close(fd);
+	if (fd < 0)
+		(perror("Open failed"), exit(1));
+	if (dup2(fd, 1) < 0)
+		(perror("Dup2 failed"), exit(1));
+	if (close(fd) < 0)
+		(perror("Close failed"), exit(1));
 }
 
 void	exec_full_command(t_node *node)
@@ -50,10 +56,18 @@ void	exec_full_command(t_node *node)
 
 void	execution(t_node *tree)
 {
+	// refacto : fork dans exec_full_command
+	// refacto : dup(1) plutot que fork comme jimmy avait voulu faire
 	pid_t	pid1;
 
 	pid1 = fork();
-	if (pid1 == 0)
-		exec_full_command(tree);
+	if (pid1 < 0)
+	{
+		(perror("Fork failed"));
+		return ;
+	}
+	else
+		if (pid1 == 0)
+			exec_full_command(tree);
 	waitpid(pid1, NULL, 0);
 }
