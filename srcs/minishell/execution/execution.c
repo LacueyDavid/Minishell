9c0d6597a7d6c612c3bfee18dadd3a800001d0c4
David LACUEY <dlacuey@paul-f4Br7s4.clusters.42paris.fr>
2024-01-03 20:44:15 +0100

    variable d'env expension, jen suis au comptage

--- srcs/minishell/execution/execution.c
+++ srcs/minishell/execution/execution.c
@@ -6,7 +6,7 @@
 /*   By: jdenis <jdenis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 21:53:01 by dlacuey           #+#    #+#             */
-/*   Updated: 2024/01/03 17:41:29 by dlacuey          ###   ########.fr       */
+/*   Updated: 2024/01/03 20:43:31 by dlacuey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,134 @@
 extern char	**environ;
 extern int	exit_status;
 
+bool	add_char(char **str, char c)
+{
+	char *new_str;
+	size_t i;
+
+	i = 0;
+	new_str = malloc(sizeof(char) * (ft_strlen(*str) + 2));
+	if (!new_str)
+		return false;
+	while ((*str)[i])
+	{
+		new_str[i] = (*str)[i];
+		i++;
+	}
+	new_str[i] = c;
+	new_str[i + 1] = '\0';
+	free(*str);
+	*str = new_str;
+	return true;
+}
+
+ssize_t count_actual_variable_size(char *value, t_envs *envs)
+{
+	printf("je rentre dans count_actual_variable_size\n");
+	size_t i = 0;
+	// size_t size = 0;
+	size_t n = 0;
+	char *dup_value = ft_strdup(value);
+	if (!dup_value)
+		return -1;
+	printf("dup_value = %s\n", dup_value);
+	while(value[n] != '"' && value[n] != '\'' && value[n] && value[n] != '$')
+		n++;
+	dup_value[n] = '\0';
+	if (!add_char(&dup_value, '='))
+		return -1;
+	printf("dup_value = %s\n", dup_value);
+	while(envs->env[i] && !ft_strnstr(envs->env[i], dup_value, ft_strlen(envs->env[i])))
+		i++;
+	free(dup_value);
+	if (!envs->env[i])
+		return 0;
+	printf("env[%ld] = %s\n", i, envs->env[i]);
+	n = 0;
+	while (envs->env[i][n] != '=')
+		n++;
+	dup_value = envs->env[i] + n + 1;
+	printf("env[%ld] = %s\n", i, envs->env[i]);
+	printf("dup_value = %s\n", dup_value);
+	printf("size de dupvalue = %zu\n", ft_strlen(dup_value));
+	return ft_strlen(dup_value);
+}
+
+ssize_t	count_final_value_size(char *value, t_envs *envs)
+{
+	(void)envs;
+	size_t i = 0;
+	size_t size = 0;
+	ssize_t variable_size;
+
+	while (value[i])
+	{
+		//le comptage ne marche pas car il faut desactiver 'asd"$a"' et
+		//"asd'$a'"
+		printf("size = %zu\n", size);
+		printf("value[%ld] = %c\n", i, value[i]);
+		if (value[i] == '$')
+		{
+			printf("je suis un dollar\n");
+			i++;
+			variable_size = count_actual_variable_size(value + i, envs);
+			if (variable_size == -1)
+				return -1;
+			size += variable_size;
+			while(value[i] != '"' && value[i] != '\'' && value[i] && value[i] != '$')
+				i++;
+		}
+		else if (value[i] == '\'')
+		{
+			i++;
+			while (value[i] != '\'')
+			{
+				size++;
+				i++;
+			}
+			i++;
+		}
+		else if (value[i] == '"')
+			i++;
+		else
+		{
+			size++;
+			i++;
+		}
+	}
+	return size + 1;
+}
+
+bool	expand_value(char **values, t_envs *envs)
+{
+	char *value = *values;
+	// char *final_value;
+	ssize_t size_of_final_value;
+	// size_t i = 0;
+
+	size_of_final_value = count_final_value_size(value, envs);
+	printf("size_of_final_value = %zu\n", size_of_final_value);
+	printf("la value = %s\n", value);
+	if (size_of_final_value == -1)
+		return false;
+	return true;
+}
+
+bool	expand_env_variables(char ***strs, t_envs *envs)
+{
+	int		i;
+	char	**values = *strs;
+
+	i = 0;
+	while (values[i])
+	{
+		if(!expand_value(&(values[i]), envs))
+			return false;
+		i++;
+	}
+	return true;
+}
+
 void	exec_in_the_son(t_node *node, t_envs *envs)
 {
 	char	**paths;
@@ -43,6 +171,12 @@ void	exec_in_the_son(t_node *node, t_envs *envs)
 		clear_tree(node->head);
 		exit(exit_status);
 	}
+	if (expand_env_variables(&(node->vector_strs.values), envs) == false)
+	{
+		(exit_status = 1, perror(RED"Expand env variables failed"WHITE));
+		clear_tree(node->head);
+		exit(exit_status);
+	}
 	wildcards(&(node->vector_strs.values));
 	if (!node->vector_strs.values)
 	{
@@ -52,8 +186,6 @@ void	exec_in_the_son(t_node *node, t_envs *envs)
 	}
 	signal(SIGQUIT, SIG_DFL);
 	signal(SIGINT, SIG_DFL);
-	for (int i = 0; node->vector_strs.values[i]; i++)
-		printf("values[%d] = %s\n", i, node->vector_strs.values[i]);
 	if (is_a_builtin(node->vector_strs.values[0]))
 	{
 		free_strs(paths);
