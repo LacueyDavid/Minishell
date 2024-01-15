78ac532bc5a4da9ee2a509e6f1c6f2e2f3068293
Jimmy DENIS <jdenis@bess-f2r5s15.clusters.42paris.fr>
2024-01-15 18:16:51 +0100

    cleaned echo

--- srcs/minishell/builtins/ft_echo.c
+++ srcs/minishell/builtins/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: jdenis <jdenis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 03:12:36 by jdenis            #+#    #+#             */
-/*   Updated: 2024/01/15 16:16:43 by jdenis           ###   ########.fr       */
+/*   Updated: 2024/01/15 18:16:48 by jdenis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,37 +34,6 @@ void	comfirm_flag(bool *is_flag, char *str, size_t *index)
 	if (str[*index] != '\0')
 		index++;
 }
-
-// int skip_n(char *str, bool *is_flag) 
-// {
-//     size_t	index;
-// 	int		index2;
-// 	bool	all_pass;
-
-// 	all_pass = false;
-// 	index = skip_first_white_space(str);
-// 	if (str[index] == '\0')
-// 		return (index);
-// 	while (all_pass == false) 
-// 	{
-// 		if(str[index] == '-')
-// 		{
-// 			index2 = index;
-// 			index++;
-// 			while (str[index] == 'n' && str[index] != '\0')
-// 				index++;
-// 			if (is_white_space(str[index]) || str[index] == '\0')
-// 				comfirm_flag(is_flag, str, &index);
-// 			else
-// 				return index2;
-// 			while (is_white_space(str[index]) && str[index] != '\0')
-// 				index++;
-// 		}
-// 		else
-// 			all_pass = true;
-//     }
-//     return (index);
-// }
 
 int	skip_n(char **strs, bool *is_flag)
 {
@@ -97,14 +66,9 @@ int ft_echo(char **command)
 {
     int index;
     bool is_flag;
-	// char *str;
 
-	// printf("str = %s\n", str);
 	is_flag = false;
-	// str = ft_compact_strs(command);
     index = skip_n(command, &is_flag);
-	// index = 1;
-	// printf("index = %d\n", index);
     while (command[index]) 
 	{
 		printf("%s", command[index]);
@@ -112,18 +76,6 @@ int ft_echo(char **command)
 			printf(" ");
 		index++;
 	}
-	// if (!is_flag)
-		// if (!is_white_space(str[index]))
-        // 	printf("%c", str[index]);
-        // if (str[index + 1] != '\0' && is_white_space(str[index + 1])) 
-		// {
-        //     while (is_white_space(str[index + 1]) && str[index + 1] != '\0')
-        //         index++;
-		// 	if (str[index + 1] != '\0')
-        //     	printf(" ");
-        // }
-        // index++;
-    // }
     if (!is_flag)
 	{
 		printf("\n");
